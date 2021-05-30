883204dd85ae96c324a2d6137aacd6ac52c91d69
Björn Tirsén <bjorntirsen@gmail.com>
2021-05-30 11:34:11 +0200

    Add connection to Atlas DB

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const database = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+//Add error handling later
+mongoose
+  .connect(database, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successfull!');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
