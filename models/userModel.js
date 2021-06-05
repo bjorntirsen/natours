d45af8bc53bef311aafc9858a7f7e260f489caf8
Björn Tirsén <bjorntirsen@gmail.com>
2021-06-05 19:43:48 +0200

    🚧Continuing on models

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -9,8 +10,22 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'A user must have an email.'],
+    unique: true,
+    lowercase: true,
+    valitate: [validator.idEmail, 'Email not valid.'],
   },
-  phoneNumber: String,
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'A user must have a password.'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'A user must confirm their password.'],
+  },
   createdAt: {
     type: Date,
     default: Date.now(),
