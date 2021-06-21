f49b1755dd29d570945ae267209b32a53f035d83
Björn Tirsén <bjorntirsen@gmail.com>
2021-06-21 10:27:28 +0200

    :recycle: Move try catch block into separate funct

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,139 +1,96 @@
-const { query } = require('express');
 const Customer = require('./../models/customerModel');
 const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync');
 
 //Customer.find Returns promise. Can also use .then here,
 //but i'm using async await.
 //With async await I need to use try catch
 //to check for errors.
-exports.getAllCustomers = async (req, res) => {
-  try {
-    console.log(req.query);
-    const features = new APIFeatures(Customer.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    const customers = await features.query;
+exports.getAllCustomers = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Customer.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const customers = await features.query;
 
-    res.status(200).json({
-      status: 'success',
-      results: customers.length,
-      data: {
-        customers,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    results: customers.length,
+    data: {
+      customers,
+    },
+  });
+});
 
-exports.getCustomer = async (req, res) => {
-  try {
-    const customer = await Customer.findById(req.params.id);
+exports.getCustomer = catchAsync(async (req, res, next) => {
+  const customer = await Customer.findById(req.params.id);
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        customer,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      customer,
+    },
+  });
+});
+
+exports.createCustomer = catchAsync(async (req, res, next) => {
+  const newCustomer = await Customer.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      customer: newCustomer,
+    },
+  });
+});
+
+exports.updateCustomer = catchAsync(async (req, res, next) => {
+  customer = await Customer.findByIdAndUpdate(req.params.id, req.body, {
+    //Return the updated customer
+    new: true,
+    //Run validation on the updates
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      customer,
+    },
+  });
+});
+
+exports.deleteCustomer = catchAsync(async (req, res, next) => {
+  await Customer.findByIdAndDelete(req.params.id);
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 
 //Below not implemented in this API
-exports.getCustomerStats = async (req, res) => {
-  try {
-    const stats = Customer.aggregate([
-      {
-        $match: { ratingsAverage: { $gte: 4.5 } },
+exports.getCustomerStats = catchAsync(async (req, res, next) => {
+  const stats = Customer.aggregate([
+    {
+      $match: { ratingsAverage: { $gte: 4.5 } },
+    },
+    {
+      $group: {
+        _id: null,
+        avgRating: { $avg: '$ratingsAverage' },
+        avgPrice: { $avg: '$price' },
+        minPrice: { $min: '$price' },
+        maxPrice: { $max: '$price' },
       },
-      {
-        $group: {
-          _id: null,
-          avgRating: { $avg: '$ratingsAverage' },
-          avgPrice: { $avg: '$price' },
-          minPrice: { $min: '$price' },
-          maxPrice: { $max: '$price' },
-        },
-      },
-    ]);
+    },
+  ]);
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        stats,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
-
-exports.createCustomer = async (req, res) => {
-  try {
-    newCustomer = await Customer.create(req.body);
-
-    res.status(201).json({
-      status: 'success',
-      data: {
-        customer: newCustomer,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
-
-exports.updateCustomer = async (req, res) => {
-  try {
-    customer = await Customer.findByIdAndUpdate(req.params.id, req.body, {
-      //Return the updated customer
-      new: true,
-      //Run validation on the updates
-      runValidators: true,
-    });
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        customer,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
-
-exports.deleteCustomer = async (req, res) => {
-  try {
-    await Customer.findByIdAndDelete(req.params.id);
-
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      stats,
+    },
+  });
+});
