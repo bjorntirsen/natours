d9911068e280feed2265eba3ebdd4d06c88ed18f
Björn Tirsén <bjorntirsen@gmail.com>
2021-06-22 10:04:29 +0200

    :goal_net: Handle invalid id DB errors

--- models/customerModel.js
+++ models/customerModel.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const customerSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A customer must have a name.'],
     unique: true,
+    maxlength: [
+      40,
+      'A customer name must have less or equal than 40 characters',
+    ],
+    minlength: [3, 'A customer name must have more or equal than 3 characters'],
   },
   organisationNr: Number,
   vatNr: String,
@@ -14,6 +20,9 @@ const customerSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'A customer must have an email.'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
   phoneNumber: String,
   createdAt: {
