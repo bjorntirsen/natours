5918f35cd12cd3f2111cd2a462ddbadd782e4969
Björn Tirsén <bjorntirsen@gmail.com>
2021-06-25 08:09:06 +0200

    :lock: Set security HTTP headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,19 +11,27 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Global Middleware
+// Set security HTTP headers
+app.use(helmet());
+
+// Dev logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit rate of requests made
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour.',
 });
-
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
