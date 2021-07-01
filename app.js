90da7ff9926f7632265bcce9f3e639f30c7d8b21
Björn Tirsén <bjorntirsen@gmail.com>
2021-07-01 12:38:08 +0200

    :recycle: Refactor views

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+const viewRouter = require('./routes/viewRoutes');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
@@ -73,25 +74,7 @@ app.use((req, res, next) => {
 });
 
 //Routes
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'the forest hiker',
-    user: 'Jonas',
-  });
-});
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours',
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
