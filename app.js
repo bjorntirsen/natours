b56e49a3f02dc9a852a5d4e8889edd20de1e375c
Björn Tirsén <bjorntirsen@gmail.com>
2021-07-02 08:41:17 +0200

    :passport_control: Add login view

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const cors = require('cors');
 
@@ -50,6 +51,7 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // Data sanitazion against NoSQL data injection
 app.use(mongoSanitize());
@@ -70,6 +72,7 @@ app.use(compression());
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(`Request made at ${req.requestTime}`);
+  console.log(req.cookies);
   next();
 });
 
--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,7 +1,7 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
-exports.getOverview = catchAsync(async (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   const tours = await Tour.find();
 
@@ -14,7 +14,7 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user',
@@ -31,3 +31,15 @@ exports.getTour = catchAsync(async (req, res) => {
       tour,
     });
 });
+
+exports.getLoginForm = (req, res) => {
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render('login', {
+      title: 'Log into your account',
+    });
+};
