7fe1ca64507ced9159e9bd7b6c3b1aecc479e498
Björn Tirsén <bjorntirsen@gmail.com>
2021-07-13 15:43:12 +0200

    :sparkles: Update user password

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -2,18 +2,20 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-export const updateData = async (name, email) => {
+export const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'password'
+        ? 'http://localhost:8000/api/v1/users/updateMyPassword'
+        : 'http://localhost:8000/api/v1/users/updateMe';
     const res = await axios({
       method: 'PATCH',
-      url: 'http://localhost:8000/api/v1/users/updateMe',
-      data: {
-        name,
-        email,
-      },
+      url,
+      data,
     });
+    
     if (res.data.status === 'success') {
-      showAlert('success', 'Data updated successfully');
+      showAlert('success', `${type.toUpperCase()} updated successfully`);
     }
   } catch (err) {
     showAlert('error', err.message);
