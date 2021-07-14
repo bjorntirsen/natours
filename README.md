a90a74605a1ab27214a66fb8166647253953520b
Björn Tirsén <bjorntirsen@gmail.com>
2021-07-14 11:30:04 +0200

    :sparkles: Upload photo from browser

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# mib-crud-api
+# Natours
+
+
--- public/js/index.js
+++ public/js/index.js
@@ -34,9 +34,13 @@ if (logOutBtn) logOutBtn.addEventListener('click', logout);
 if (userDataForm)
   userDataForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    updateSettings({ name, email }, 'data');
+    const form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+    console.log(form);
+
+    updateSettings(form, 'data');
   });
 
 if (userPasswordForm)
@@ -46,7 +50,10 @@ if (userPasswordForm)
     const passwordCurrent = document.getElementById('password-current').value;
     const password = document.getElementById('password').value;
     const passwordConfirm = document.getElementById('password-confirm').value;
-    await updateSettings({ passwordCurrent, password, passwordConfirm }, 'password');
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
 
     document.querySelector('.btn--save-password').textContent = 'Save password';
     document.getElementById('password-current').value = '';
