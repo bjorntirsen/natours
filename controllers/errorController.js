8a95f245de8aa0c8b1ee7daae039e75cb5da26ea
Björn Tirsén <bjorntirsen@gmail.com>
2021-07-14 10:56:09 +0200

    :sparkles: Format images using sharp

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -16,7 +16,6 @@ const handleDuplicateFieldsDB = (err) => {
 const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
   const message = `Invalid input data. ${errors.join('. ')}`;
-  console.log(message);
   return new AppError(message, 400);
 };
 
