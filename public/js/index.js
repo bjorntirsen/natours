a434e467379d0eab941c0e7dd32ee4fc11c51f5c
Björn Tirsén <bjorntirsen@gmail.com>
2021-07-16 08:24:28 +0200

    :sparkles: Add alert to booking

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,7 @@ import { login, logout } from './login';
 import { displayMap } from './mapbox';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 // DOM elements
 const mapBox = document.getElementById('map');
@@ -64,7 +65,10 @@ if (userPasswordForm)
 
 if (bookBtn)
   bookBtn.addEventListener('click', (e) => {
-    e.target.textContent = 'Processing...'
+    e.target.textContent = 'Processing...';
     const { tourId } = e.target.dataset;
     bookTour(tourId);
   });
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 10);
