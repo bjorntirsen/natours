accc90022fae2ff0bdcb71bcc56dbec22c98e69f
Björn Tirsén <bjorntirsen@gmail.com>
2021-07-16 09:25:23 +0200

    :bug: Fix logout bug

--- public/js/login.js
+++ public/js/login.js
@@ -28,10 +28,11 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: '/api/v1/users/logout'
+      url: '/api/v1/users/logout',
     });
-    if ((res.data.status = 'success')) location.relod(true);
+    if ((res.data.status = 'success')) location.reload();
   } catch (err) {
+    console.log(err.response);
     showAlert('error', 'Error logging out! Try again.');
   }
 };
